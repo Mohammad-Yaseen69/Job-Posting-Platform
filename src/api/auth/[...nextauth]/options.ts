afebc524f9bb42c00201597dcc35773636014b11
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2026-01-28 23:32:56 +0500

    feat: add next-auth options with github provider and jwt

---
+++ src/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
+
+const { } = NextAuth({
+    providers: [
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.AUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+        jwt: ({ token, user }) => {
+            if (user) {
+                token.id = user.id
+                token.name = user.name
+            }
+
+            return token
+        },
+        session: ({ session, token }) => {
+            if (session.user) {
+                session.user.id = token.id as string
+                session.user.name = token.name as string
+            }
+            return session
+        }
+    }
+})
