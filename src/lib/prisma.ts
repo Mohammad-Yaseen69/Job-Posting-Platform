fea92e96f73932ca5f845e4ecabea235118a6b3f
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2026-01-31 18:02:47 +0500

    feat: update prisma client lib file with adapter-pg

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,18 @@
 import { PrismaClient } from '@/generated/prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: ReturnType<typeof createPrismaClient>
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
 }
 
-function createPrismaClient() {
-  return new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL!
-  }).$extends(withAccelerate())
-}
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-export const prisma =
-  globalForPrisma.prisma ?? createPrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
